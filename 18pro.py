fbba83bdbc0b852e6258f1527e534b726b47de26
umadevic <51950421+umadevic@users.noreply.github.com>
2019-07-24 22:12:45 +0530

    Update and rename README.md to 18pro.py

---
+++ 18pro.py
@@ -0,0 +1,16 @@
+oii,hii=map(int,input().split())
+tem=[]
+x=0
+for m in range(oii):
+    tem.append(list(map(int,input().split())))   
+for m in range(oii):
+    for j in range(hii-1):
+        for k in range(j+1,hii+1):
+            if tem[m][j:k]==[1]*len(tem[m][j:k]):
+                 if all(tem[p+m][j:k]==[1]*len(tem[p+m][j:k]) for p in range(len(tem[m][j:k])-1)):
+                     if len(tem[m][j:k])>x:
+                        x=len(tem[m][j:k])
+if len(tem)==1 and len(tem[0])==1 and tem[0][0]==1:
+    print(1)
+for m in range(x):
+    print(*[1]*x) 
