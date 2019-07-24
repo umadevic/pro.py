4c3c20eb60d2dd20042a4bc6afee21580961ede1
umadevic <51950421+umadevic@users.noreply.github.com>
2019-07-24 22:50:55 +0530

    Update 22pro.py

--- 22pro.py
+++ 22pro.py
@@ -1,16 +1,16 @@
 #a
-value=int(input())
+count1=int(input())
 arr=list(map(int,input().split()))
 arr.sort(reverse=True)
 #print(arr)
 sum=0
 sum1=0
-res1=[]
-for i in range(0,value,2):
+res2=[]
+for i in range(0,count1,2):
     sum=sum+arr[i]
-for j in range(1,value,2):
+for j in range(1,count1,2):
     sum1=sum1+arr[j]
-res1.append([sum,sum1])
-#print(res1)
-for i in res1:
+res2.append([sum,sum1])
+#print(res)
+for i in res2:
     print(i[0] if i[0]>i[1] else i[1])
